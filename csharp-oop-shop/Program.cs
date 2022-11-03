eb37ba05ef1baaf57301683784f173c67a76d541
Debby95 <debbyjacksonstar@hotmail.it>
2022-11-03 11:37:18 +0100

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop. Un prodotto è caratterizzato da:
+//codice(numero intero)
+//nome
+//descrizione
+//prezzo
+//iva
+
+using System.Net.Http.Headers;
+
+//variabili globali
+ProductHeaderValue product = new Product();
+
+//programma principale 
+product.Code = 40;
+product.Name = "Keyboard";
+product.Description = "Gaming Keyboard";
+product.Price = 180;
+product.Iva = 10;
+
+
+
